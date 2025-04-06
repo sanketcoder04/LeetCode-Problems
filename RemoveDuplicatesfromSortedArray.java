0fc01a76e84c3aaf975b62ec4f724546b3c026ef
Sanket Ganguly <sanketganguly53@gmail.com>
2025-04-06 19:28:17 +0530

    Update RemoveDuplicatesfromSortedArray.java

--- RemoveDuplicatesfromSortedArray.java
+++ RemoveDuplicatesfromSortedArray.java
@@ -36,14 +36,12 @@
 class RemoveDuplicatesfromSortedArray {
     public int removeDuplicates(int[] nums) {
         int index = 0;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] != nums[i+1]){
-                nums[index] = nums[i];
+        for(int i=1;i<nums.length;i++){
+            if(nums[i] != nums[index]){
+                nums[index + 1] = nums[i];
                 index++;
             }
         }
-        nums[index] = nums[nums.length-1];
-        index++;
-        return index;
+        return index + 1;
     }
 }

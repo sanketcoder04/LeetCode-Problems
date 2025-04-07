8897099180717d3f42c1871095886e231885ce67
Sanket Ganguly <sanketganguly53@gmail.com>
2025-04-07 16:43:31 +0530

    Create AddDigits.java

---
+++ AddDigits.java
@@ -0,0 +1,35 @@
+// Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+// Example 1:
+// Input: num = 38
+// Output: 2
+// Explanation: The process is
+// 38 --> 3 + 8 --> 11
+// 11 --> 1 + 1 --> 2 
+// Since 2 has only one digit, return it.
+  
+// Example 2:
+// Input: num = 0
+// Output: 0
+ 
+// Constraints:
+// 0 <= num <= 231 - 1
+
+class AddDigits {
+    public int addDigits(int num) {
+        while(num / 10 != 0){
+            num = getDigitSum(num);
+        }
+        return num;
+    }
+  
+    private int getDigitSum(int n){
+        int sum = 0;
+        while(n != 0){
+            int rem = n % 10;
+            sum += rem;
+            n = n / 10;
+        }
+        return sum;
+    }
+}

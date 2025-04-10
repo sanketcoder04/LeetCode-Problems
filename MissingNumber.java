7926e7b8b93ba7d01c015310d87372e0a7b9ed00
Sanket Ganguly <sanketganguly53@gmail.com>
2025-04-10 20:56:09 +0530

    Update MissingNumber.java

--- MissingNumber.java
+++ MissingNumber.java
@@ -60,4 +60,17 @@ class Solution {
         }
         return nums.length;
     }
+
+    // Using XOR Operation
+
+    public int missingNumber(int[] nums){
+        int xor1 = 0;
+        int xor2 = 0;
+        for(int i=0;i<nums.length;i++) {
+            xor1 = xor1 ^ nums[i];
+            xor2 = xor2 ^ i;
+        }
+        xor2 = xor2 ^ nums.length;
+        return xor1 ^ xor2;
+    }
 }

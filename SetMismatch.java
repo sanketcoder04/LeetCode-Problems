7425ff3e68a401f55688ed3a84ea4c1750cebc54
Sanket Ganguly <sanketganguly53@gmail.com>
2025-05-12 12:13:34 +0530

    Create SetMismatch.java

---
+++ SetMismatch.java
@@ -0,0 +1,42 @@
+// You have a set of integers s, which originally contains all the numbers from 1 to n. 
+// Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+// You are given an integer array nums representing the data status of this set after the error.
+// Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+// Example 1:
+// Input: nums = [1,2,2,4]
+// Output: [2,3]
+  
+// Example 2:
+// Input: nums = [1,1]
+// Output: [1,2]
+ 
+// Constraints:
+// 2 <= nums.length <= 104
+// 1 <= nums[i] <= 104
+
+class SetMismatch {
+    public int[] findErrorNums(int[] nums) {
+        int[] result = new int[2];
+        int index = 0;
+        while(index < nums.length){
+            int correct = nums[index]-1;
+            if(nums[index] != nums[correct]){
+                int temp = nums[index];
+                nums[index] = nums[correct];
+                nums[correct] = temp;
+            }
+            else{
+                index++;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] != i+1){
+                result[0] = nums[i];
+                result[1] = i+1;
+                break;
+            }
+        }
+        return result;
+    }
+}

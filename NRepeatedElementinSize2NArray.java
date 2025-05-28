e05c91edbd59a42e8da3b98c5f94c60935acf86a
Sanket Ganguly <sanketganguly53@gmail.com>
2025-05-28 14:08:38 +0530

    Create NRepeatedElementinSize2NArray.java

---
+++ NRepeatedElementinSize2NArray.java
@@ -0,0 +1,46 @@
+// You are given an integer array nums with the following properties:
+// nums.length == 2 * n.
+// nums contains n + 1 unique elements.
+// Exactly one element of nums is repeated n times.
+// Return the element that is repeated n times.
+
+// Example 1:
+// Input: nums = [1,2,3,3]
+// Output: 3
+  
+// Example 2:
+// Input: nums = [2,1,2,5,3,2]
+// Output: 2
+  
+// Example 3:
+// Input: nums = [5,1,5,2,5,3,5,4]
+// Output: 5
+ 
+// Constraints:
+// 2 <= n <= 5000
+// nums.length == 2 * n
+// 0 <= nums[i] <= 104
+// nums contains n + 1 unique elements and one of them is repeated exactly n times.
+
+class NRepeatedElementinSize2NArray {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length / 2;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums) {
+            if(map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            }
+            else {
+                map.put(num, 1);
+            }
+        }
+        for(Integer key : map.keySet()) {
+            Integer value = map.get(key);
+            if(value == n) {
+                return key;
+            }
+        }
+        return -1;
+    }
+}

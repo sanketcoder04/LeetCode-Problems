2d35f50473841d1792da51f487af46987b0fee37
Sanket Ganguly <sanketganguly53@gmail.com>
2025-06-11 13:41:25 +0530

    Create BinaryStringWithSubstringsRepresenting1ToN.java

---
+++ BinaryStringWithSubstringsRepresenting1ToN.java
@@ -0,0 +1,27 @@
+// Given a binary string s and a positive integer n, return true if the binary representation of all the integers in the range [1, n] are substrings of s, or false otherwise.
+// A substring is a contiguous sequence of characters within a string.
+
+// Example 1:
+// Input: s = "0110", n = 3
+// Output: true
+  
+// Example 2:
+// Input: s = "0110", n = 4
+// Output: false
+ 
+// Constraints:
+// 1 <= s.length <= 1000
+// s[i] is either '0' or '1'.
+// 1 <= n <= 109
+
+class BinaryStringWithSubstringsRepresenting1ToN {
+    public boolean queryString(String s, int n) {
+        for(int i=1;i<=n;i++) {
+            String binary = Integer.toBinaryString(i);
+            if(!s.contains(binary)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

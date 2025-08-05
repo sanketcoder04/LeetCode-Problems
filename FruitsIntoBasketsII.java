7ca18158c17498a7b66097bca0aaf33740c0abfc
Sanket Ganguly <sanketganguly53@gmail.com>
2025-08-05 11:49:37 +0530

    Create FruitsIntoBasketsII.java

---
+++ FruitsIntoBasketsII.java
@@ -0,0 +1,53 @@
+// You are given two arrays of integers, fruits and baskets, each of length n, 
+// where fruits[i] represents the quantity of the ith type of fruit, and baskets[j] represents the capacity of the jth basket.
+// From left to right, place the fruits according to these rules:
+// Each fruit type must be placed in the leftmost available basket with a capacity greater than or equal to the quantity of that fruit type.
+// Each basket can hold only one type of fruit.
+// If a fruit type cannot be placed in any basket, it remains unplaced.
+// Return the number of fruit types that remain unplaced after all possible allocations are made.
+
+// Example 1:
+// Input: fruits = [4,2,5], baskets = [3,5,4]
+// Output: 1
+// Explanation:
+// fruits[0] = 4 is placed in baskets[1] = 5.
+// fruits[1] = 2 is placed in baskets[0] = 3.
+// fruits[2] = 5 cannot be placed in baskets[2] = 4.
+// Since one fruit type remains unplaced, we return 1.
+
+// Example 2:
+// Input: fruits = [3,6,1], baskets = [6,4,7]
+// Output: 0
+// Explanation:
+// fruits[0] = 3 is placed in baskets[0] = 6.
+// fruits[1] = 6 cannot be placed in baskets[1] = 4 (insufficient capacity) but can be placed in the next available basket, baskets[2] = 7.
+// fruits[2] = 1 is placed in baskets[1] = 4.
+// Since all fruits are successfully placed, we return 0.
+
+// Constraints:
+// n == fruits.length == baskets.length
+// 1 <= n <= 100
+// 1 <= fruits[i], baskets[i] <= 1000
+
+class FruitsIntoBasketsII {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int count = 0;
+        boolean[] isPlaced = new boolean[baskets.length];   // use to check that current basket already occupied
+        for(int i=0;i<fruits.length;i++) {
+            int fruit = fruits[i];
+            boolean fruitTrack = false;     // use to track that current fruit is placed or not in any basket
+            for(int j=0;j<baskets.length;j++) {
+                if(!isPlaced[j] && baskets[j] >= fruit) {
+                    baskets[j] -= fruit;
+                    isPlaced[j] = true;
+                    fruitTrack = true;
+                    break;
+                }
+            }
+            if(!fruitTrack) {
+                count++;    // if current fruit is not fit in any basket then increase the count
+            }
+        }
+        return count;
+    }
+}

752e26e8bfd6d3ad792e563d9990b4678bbb32ac
Sanket Ganguly <sanketganguly53@gmail.com>
2025-08-13 12:33:13 +0530

    Update PowerofThree.java

--- PowerofThree.java
+++ PowerofThree.java
@@ -21,13 +21,13 @@
 
 class PowerofThree {
     public boolean isPowerOfThree(int n) {
-        if(n <= 0){
+        if(n <= 0) {
             return false;
         }
-        if(n == 1){
+        if(n == 1) {
             return true;
         }
-        if(n % 3 != 0){
+        if(n % 3 != 0) {
             return false;
         }
         return isPowerOfThree(n / 3);

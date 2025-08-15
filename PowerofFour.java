e3959360e6ddcad833a92963e267b13dc68c87c3
Sanket Ganguly <sanketganguly53@gmail.com>
2025-08-15 19:21:28 +0530

    Update PowerofFour.java

--- PowerofFour.java
+++ PowerofFour.java
@@ -18,13 +18,13 @@
 
 class PowerofFour {
     public boolean isPowerOfFour(int n) {
-        if(n <= 0){
+        if(n <= 0) {
             return false;
         }
-        if(n == 1){
+        if(n == 1) {
             return true;
         }
-        if(n % 4 != 0){
+        if(n % 4 != 0) {
             return false;
         }
         return isPowerOfFour(n / 4);

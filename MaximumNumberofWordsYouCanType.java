4dc2e8ffb774e6446557982dca6f891695763152
Sanket Ganguly <sanketganguly53@gmail.com>
2025-09-16 00:34:55 +0530

    Create MaximumNumberofWordsYouCanType.java

---
+++ MaximumNumberofWordsYouCanType.java
@@ -0,0 +1,53 @@
+// There is a malfunctioning keyboard where some letter keys do not work. All other keys on the keyboard work properly.
+// Given a string text of words separated by a single space (no leading or trailing spaces) 
+// and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
+
+// Example 1:
+// Input: text = "hello world", brokenLetters = "ad"
+// Output: 1
+// Explanation: We cannot type "world" because the 'd' key is broken.
+  
+// Example 2:
+// Input: text = "leet code", brokenLetters = "lt"
+// Output: 1
+// Explanation: We cannot type "leet" because the 'l' and 't' keys are broken.
+  
+// Example 3:
+// Input: text = "leet code", brokenLetters = "e"
+// Output: 0
+// Explanation: We cannot type either word because the 'e' key is broken.
+ 
+// Constraints:
+// 1 <= text.length <= 104
+// 0 <= brokenLetters.length <= 26
+// text consists of words separated by a single space without any leading or trailing spaces.
+// Each word only consists of lowercase English letters.
+// brokenLetters consists of distinct lowercase English letters.
+
+class MaximumNumberofWordsYouCanType {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        char[] letters = brokenLetters.toCharArray();
+        Set<Character> brokenLetter = new HashSet<>();
+        for(char letter : letters) {
+            brokenLetter.add(letter);
+        }
+        String[] words = text.split(" ");
+        int ans = 0;
+
+        for(String word : words) {
+            if(checkIfPossible(word, brokenLetter)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+
+    private boolean checkIfPossible(String word, Set<Character> brokenLetter) {
+        for(char ch : word.toCharArray()) {
+            if(brokenLetter.contains(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
